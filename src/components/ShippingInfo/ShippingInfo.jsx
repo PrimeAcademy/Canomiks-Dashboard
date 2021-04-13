a62dc1ca09124b4a0bfaf8f3cfbd38fb2c254c23
michaelshaleen <shaleenmichael@yahoo.com>
2021-04-13 16:30:57 -0500

    inputs are required, inputs have values using local state, functionality adding to buttons

--- src/components/ShippingInfo/ShippingInfo.jsx
+++ src/components/ShippingInfo/ShippingInfo.jsx
@@ -1,22 +1,78 @@
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
+
+
 
 
 function ShippingInfo(){
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [shipDate, setDate] = useState('');
+  const [carrier, setCarrier] = useState('');
+  const [tracking, setTracking] = useState('');
 
     function handleSubmit(event){
-        event.preventDefault()
+      event.preventDefault()
+      if(!shipDate || !carrier || !tracking){
+        console.log("no inputs")
+        alert("All inputs are required")
+      }
+      else{
+
+        
         history.push("/dashboard")
+      }
+
+    }
+
+
+    const continueLater = () => {
+      console.log("continue later")
+      alert("Order cannot be processed until shipping info entered")
     }
     
     return(
         <>
         <form>
-            <p> These are the available shipping dates. Samples cannot be processed until shipping info is filled out</p>
-           <div> Date to be shipped: <input type="text" placeholder="MM/DD/YY"/> </div>
-           <div> Carrier: <input type="text" placeholder="Not Specified" /></div>
-           <div> Tracking Number: <input type="text" /> </div>
+            <p> 
+              These are the available shipping dates. 
+              Samples cannot be processed until shipping info is filled out
+            </p>
+           <div>
+              Date to be shipped: 
+              <input 
+                value={shipDate} 
+                type="text" 
+                placeholder="MM/DD/YY"
+                onChange={(event) => setDate(event.target.value)}
+              /> 
+           </div>
+
+           <div>
+              Carrier: 
+              <input 
+                value={carrier} 
+                type="text" 
+                placeholder="Not Specified" 
+                onChange={(event) => setCarrier(event.target.value)}
+              />
+            </div>
+
+           <div> 
+             Tracking Number: 
+             <input 
+              value={tracking} 
+              type="text"
+              onChange={(event) => setTracking(event.target.value)}
+              /> 
+           </div>
+
         </form>
-        <span> <button > Back </button><button > Continue Later </button><button onClick={handleSubmit}> Finalize</button></span>
+        <span> <button > Back </button>
+        <button onClick={continueLater}> Continue Later </button>
+        <button onClick={handleSubmit}> Finalize</button></span>
         </>
 
     )
