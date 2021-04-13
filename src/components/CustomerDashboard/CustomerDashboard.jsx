6a1f68d4bb58f4c3500923f36e1800925db0a9b4
George Papagapitos <george.g.papagapitos@gmail.com>
2021-04-13 18:50:06 -0500

    centered table and added maxheight

--- src/components/CustomerDashboard/CustomerDashboard.jsx
+++ src/components/CustomerDashboard/CustomerDashboard.jsx
@@ -33,45 +33,44 @@ export default function CustomerDashboard() {
         <Typography variant="h4" gutterBottom>COMPANY ID: {user.companyID}</Typography>
         <Button variant="contained" style={{ backgroundColor: "#1e565c", color: "white" }} onClick={() => history.push('/addSample')}>+ SAMPLE</Button>
         <div>
-          <TextField style={{ margin: 75 }} onChange={(event) => { setFilter(event.target.value) }} label="Search..." variant="standard" />
+          <TextField style={{ margin: 25 }} onChange={(event) => { setFilter(event.target.value) }} label="Search..." variant="standard" />
         </div>
+        <TableContainer style={{ maxWidth: '80%', maxHeight: 450 }} component={Paper}>
+          <Table stickyHeader className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontWeight: 900 }}>Lot ID</TableCell>
+                <TableCell align="right" style={{ fontWeight: 900 }}>Ingredient Name</TableCell>
+                <TableCell align="right" style={{ fontWeight: 900 }}>Date Received</TableCell>
+                <TableCell align="right" style={{ fontWeight: 900 }}>Test Phase</TableCell>
+                <TableCell align="right" style={{ fontWeight: 900 }}>Details</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.map((order) => {
+                if (order.lotNumber.toLowerCase().includes(filter.toLowerCase())) {
+                  return (
+                    <TableRow style={{ backgroundColor: order.testingStatus === 'Pre-shipment' && '#F3A653' }} key={order.id}>
+                      <TableCell component="th" scope="row">
+                        {order.lotNumber}
+                      </TableCell>
+                      <TableCell align="right">{order.ingredientName} - {order.cropStrain}</TableCell>
+                      {order.receivedDate ?
+                        <TableCell align="right">{moment(order.receivedDate).format('MMMM DD YYYY')}</TableCell> :
+                        <TableCell align="right">Not Shipped</TableCell>
+                      }
+                      <TableCell align="right">{order.testingStatus}</TableCell>
+                      <TableCell align="right">
+                        {order.testingStatus === 'Pre-shipment' ? <Button variant="contained" style={{ backgroundColor: 'white', color: '#1e565c' }}>Add Shipping Info</Button> : <Button variant="contained" style={{ backgroundColor: '#1e565c', color: 'white' }}>View Details</Button>}
+                      </TableCell>
+                    </TableRow>
+                  )
+                }
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </center>
-      <Divider />
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Lot #</TableCell>
-              <TableCell align="right">Ingredient Name</TableCell>
-              <TableCell align="right">Date Received</TableCell>
-              <TableCell align="right">Test Phase</TableCell>
-              <TableCell align="right">Details</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orders.map((order) => {
-              if (order.lotNumber.toLowerCase().includes(filter.toLowerCase())) {
-                return (
-                  <TableRow style={{ backgroundColor: order.testingStatus === 'Pre-shipment' && '#F3A653' }} key={order.id}>
-                    <TableCell component="th" scope="row">
-                      Lot #{order.lotNumber}
-                    </TableCell>
-                    <TableCell align="right">{order.ingredientName} - {order.cropStrain}</TableCell>
-                    {order.receivedDate ?
-                      <TableCell align="right">{moment(order.receivedDate).format('MMMM DD YYYY')}</TableCell> :
-                      <TableCell align="right">Not Shipped</TableCell>
-                    }
-                    <TableCell align="right">{order.testingStatus}</TableCell>
-                    <TableCell align="right">
-                      {order.testingStatus === 'Pre-shipment' ? <Button variant="contained" style={{ backgroundColor: 'white', color: '#1e565c' }}>Add Shipping Info</Button> : <Button variant="contained" style={{ backgroundColor: '#1e565c', color: 'white' }}>View Details</Button>}
-                    </TableCell>
-                  </TableRow>
-                )
-              }
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
     </>
   );
 }
