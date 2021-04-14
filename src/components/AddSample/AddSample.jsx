3d1617f50ecfff84a38192cb7c404140c6252e21
michaelshaleen <shaleenmichael@yahoo.com>
2021-04-14 14:46:25 -0500

    cancel clears

--- src/components/AddSample/AddSample.jsx
+++ src/components/AddSample/AddSample.jsx
@@ -10,6 +10,9 @@ function AddSample() {
   const [dateManufactured, setDate] = useState('');
   const [extractionMethod, setMethod] = useState('');
   const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [harvestDate, setHarvestDate] = useState('');
   const [sustainability, setSustainability] = useState('');
   const [strain, setStrain] = useState('');
 
@@ -26,91 +29,142 @@ function AddSample() {
     console.log('shipping info')
   }
 
+
+  const cancel = (event) => {
+    console.log("cancel")
+    setName('');
+    setLotNumber('');
+    setFormat('');
+    setPurity('');
+    setDate('');
+    setMethod('');
+    setCity('');
+    setState('');
+    setCountry('');
+    setStrain('');
+    setHarvestDate('');
+    setSustainability('');
+  }
+
   return(<>
   <div>
     <form onSubmit={handleSubmit}>
-      <input 
-        type="text"
-        value={ingredientName}>
+      <div>
         Ingredient Name*:
-      </input>
-      <input 
-        type="text"
-        value={lotNumber}>
-        Lot# *:
-      </input>
-
-      <select 
-        value={productFormat}
-        onChange={(event) => setFormat(event.target.value)}
-        >Product Format*:
-          <option>Tincture</option>
-          <option>Oil</option>
-          <option>Other</option>
-      </select>
-
-      <select
-        value={ingredientAmount}
-        onChange={(event) => setAmount(event.target.value)}>
-        Ingredient Amount*:
-          <option></option>
-          <select>
-            <option>Grams</option>
-            <option>Ounces</option>
+          <input 
+            value={ingredientName}
+            type="text"
+            placeholder="Name"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          Lot# *:
+          <input
+           value={lotNumber}
+           type="text"
+           placeholder="Lot Number"
+           onChange={(event) => setLotNumber(event.target.value)}
+           />
+        </div>
+        <div>
+        Product Format*:
+          <select 
+            value={productFormat}
+            onChange={(event) => setFormat(event.target.value)}
+            >
+              <option value="Powder">Powder</option>
+              <option value="Tincture">Tincture</option>
+              <option value="Oil">Oil</option>
+              <option value="Other">Other</option>
           </select>
-      </select>
+          </div>
 
-      <input
-        type="text"
-        value={purity}
-        onChange={(event) => setPurity(event.target.value)}>
-          Purity(If Available)
-      </input>
+          <div
+            value={ingredientAmount}
+            onChange={(event) => setAmount(event.target.value)}>
+            Ingredient Amount*:
+            <input  
+              type="number"/>
+            <select>
+              <option>Milligrams</option>
+              <option>Grams</option>
+              <option>Ounces</option>
+            </select>
+          </div>
 
-      <input 
-        type="date"
-        value={dateManufactured}
-        onChange={(event) => setDate(event.target.value)}>
-          Date Manufactured*
-      </input>
-      <input 
-        type="text"
-        value={extractionMethod}
-        onChange={(event) => setMethod(event.target.value)}>
-          Extraction Method:
-      </input>
+          <div>
+            Purity(If Available)
+            <input
+              type="text"
+              value={purity}
+              onChange={(event) => setPurity(event.target.value)}/>
+          </div>
+          <div>
+            Date Manufactured*
+            <input 
+              type="date"
+              value={dateManufactured}
+              onChange={(event) => setDate(event.target.value)}/>
+        </div>
+        <div>
+        Extraction Method*:
+            <input 
+              type="text"
+              value={extractionMethod}
+              onChange={(event) => setMethod(event.target.value)}/>
+        </div>
+     
       <div>
         Growth Region
-        <input 
-          type="text"
-          value={city}
-          onChange={(event) => setCity(event.target.value)}>
-          City:
-        </input>
-
+        <div>
+              City:
+            <input 
+              type="text"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}/>
+              State:
+              <input 
+              type="text"
+              value={state}
+              onChange={(event) => setState(event.target.value)}/>
+              Country:
+              <input 
+              type="text"
+              value={country}
+              onChange={(event) => setCountry(event.target.value)}/>
+          </div>
       </div>
 
-      <input
-        type="text"
-        value={strain}
-        onChange={(event) => setStrain(event.target.value)}>
-        Strain Of Crop:
-      </input>
 
-        <select>Harvest Date</select>
-      
+      <div>
+          Strain Of Crop:
+          <input
+            type="text"
+            value={strain}
+            onChange={(event) => setStrain(event.target.value)}/>
+      </div>
+      <div
+        value={harvestDate}
+        onChange={(event) => setHarvestDate(event.target.value)}>
+        Harvest Date
+        <input
+          type="month" />
+      </div>
 
-      <input
-        type="text"
-        value={sustainability}
-        onChange={(event) => setSustainability(event.target.value)}>
-          Sustainability Info:
-        </input>
+      <div>
+        Sustainability Info:
+        <input  
+          value={sustainability}
+          type="text"
+          onChange={(event) => setSustainability(event.target.value)}/>   
+      </div>
 
-        <button>Cancel Request</button>
         <button onClick={shipping}>Shipping Info</button>
-        
     </form>
+        <button onClick={cancel}>Cancel Request</button>
+        
+    
   </div>  
   
   </>)
