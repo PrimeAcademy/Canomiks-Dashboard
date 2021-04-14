96d584d743677ec971a158644addbf872b9d3b18
George Papagapitos <george.g.papagapitos@gmail.com>
2021-04-14 14:13:10 -0500

    removed duplicate form in shipping info

--- src/components/ShippingInfo/ShippingInfo.jsx
+++ src/components/ShippingInfo/ShippingInfo.jsx
@@ -6,7 +6,7 @@ function ShippingInfo() {
   const history = useHistory();
   const dispatch = useDispatch();
   const [carrierName, setCarrierName] = useState('');
-  const [trackingNumber, setTrackingNumber] = useState(1)
+  const [trackingNumber, setTrackingNumber] = useState('')
   const [shippedDate, setDate] = useState('');
 
   function handleSubmit(event) {
@@ -27,87 +27,62 @@ function ShippingInfo() {
       alert("Shipping Successful!")
       history.push("/dashboard")
     }
-
-  }
-
-  const continueLater = () => {
-    console.log("continue later")
-    alert("Order cannot be processed until shipping info entered")
   }
 
   const backBtn = () => {
     console.log("back button")
-    history.push("/samples")
+    history.push("/addSamples")
   }
-  function handleBack(event) {
-    event.preventDefault()
-    history.push("/")
-  }
+
   function handleContinue(event) {
     event.preventDefault();
     alert("Sample cannot be processed until shipping information is entered")
     history.push("/")
   }
-  function handleSubmit(event) {
-    event.preventDefault()
-    dispatch({
-      type: 'ADD_SHIPPING',
-      payload: {
-        shippedDate: date,
-        carrierName: carrier,
-        trackingNumber: tracking
-      }
-    })
-    history.push('/')
-  }
+
 
   return (
     <>
-      <form>
-        <p> These are the available shipping dates. Samples cannot be processed until shipping info is filled out</p>
-        <div> Date to be shipped: <input type="text" placeholder="MM/DD/YY" onChange={(evt) => { setDate(evt.target.value) }} /> </div>
-        <div> Carrier: <input type="text" placeholder="Not Specified" onChange={(evt) => { setCarrierName(evt.target.value) }} /></div>
-        <div> Tracking Number: <input type="text" onChange={(evt) => { setTracking(evt.target.value) }} /> </div>
-
-        <span> <button onClick={handleBack}> Back to add sample </button><button onClick={handleContinue}> Continue Later </button><button onClick={handleSubmit}> Finalize</button></span>
-
-        <p>
-          These are the available shipping dates.
-          Samples cannot be processed until shipping info is filled out
+      <center>
+        <form>
+          <p>
+            These are the available shipping dates.
+            Samples cannot be processed until shipping info is filled out
             </p>
-        <div>
-          Date to be shipped:
+          <div>
+            Date to be shipped:
               <input
-            value={shippedDate}
-            type="text"
-            placeholder="MM/DD/YY"
-            onChange={(event) => setDate(event.target.value)}
-          />
-        </div>
-        <div>
-          Carrier:
+              value={shippedDate}
+              type="text"
+              placeholder="MM/DD/YY"
+              onChange={(event) => setDate(event.target.value)}
+            />
+          </div>
+          <div>
+            Carrier:
               <input
-            value={carrierName}
-            type="text"
-            placeholder="Not Specified"
-            onChange={(event) => setCarrierName(event.target.value)}
-          />
-        </div>
+              value={carrierName}
+              type="text"
+              placeholder="Not Specified"
+              onChange={(event) => setCarrierName(event.target.value)}
+            />
+          </div>
 
-        <div>
-          Tracking Number:
+          <div>
+            Tracking Number:
              <input
-            value={trackingNumber}
-            type="text"
-            onChange={(event) => setTrackingNumber(event.target.value)}
-          />
-        </div>
-      </form>
-      <span>
-        <button onClick={backBtn}> Back </button>
-        <button onClick={continueLater}> Continue Later </button>
-        <button onClick={handleSubmit}> Finalize</button>
-      </span>
+              value={trackingNumber}
+              type="text"
+              onChange={(event) => setTrackingNumber(event.target.value)}
+            />
+          </div>
+        </form>
+        <span>
+          <button onClick={backBtn}> Back </button>
+          <button onClick={handleContinue}> Continue Later </button>
+          <button onClick={handleSubmit}> Finalize</button>
+        </span>
+      </center>
     </>
   )
 }
