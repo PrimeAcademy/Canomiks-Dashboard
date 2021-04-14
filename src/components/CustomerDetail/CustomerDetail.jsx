7fffca3bd55a8cbe2f9361cbd603a311f3d60924
Vada Karlen <vada.karlen@gmail.com>
2021-04-14 10:12:29 -0500

    Modal displays sample info

--- src/components/CustomerDetail/CustomerDetail.jsx
+++ src/components/CustomerDetail/CustomerDetail.jsx
@@ -1,9 +1,31 @@
 import { DialogContent, DialogContentText } from '@material-ui/core';
 
-function CustomerDetail(params) {
+function CustomerDetail({ sample }) {
   return (
     <DialogContent>
-      <DialogContentText>This box</DialogContentText>
+      <DialogContentText>
+        <h2>Lot # {sample.lotNumber}</h2>
+        <h3>{sample.ingredientName}</h3>
+
+        <div>
+          <p>
+            Amount: {sample.ingredientAmount} {sample.ingredientUnit}
+          </p>
+          <p>Format: {sample.format}</p>
+          <p>Purity: {sample.purity}</p>
+          <p>Strain: {sample.cropStrain}</p>
+        </div>
+
+        <div>
+          <p>Manufacture Date: {sample.dateManufactured}</p>
+          <p>Extraction Method: {sample.extractionMethod}</p>
+          <p>
+            Growth Region: {sample.city}, {sample.state}, {sample.country}
+          </p>
+          <p>Harvest Date: {sample.harvestDate}</p>
+          <p>Sustainability: {sample.sustainabilityInfo}</p>
+        </div>
+      </DialogContentText>
     </DialogContent>
   );
 }
