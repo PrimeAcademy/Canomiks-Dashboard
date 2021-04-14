031f70b92a73fe936ed891cbbc53c8f23f060ce5
Vada Karlen <vada.karlen@gmail.com>
2021-04-14 08:52:35 -0500

    Clicking View Details brings up modal

--- server/routes/order.router.js
+++ server/routes/order.router.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
 router.get('/', rejectUnauthenticated, async (req, res) => {
-  // GET route code here
   try {
     const queryText = `SELECT * FROM "orders" WHERE orders."companyID" = $1;`;
     const dbRes = await pool.query(queryText, [req.user.companyID]);
     res.send(dbRes.rows);
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err.message);
     res.sendStatus(500);
   }
@@ -23,15 +23,27 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
  * POST route template
  */
 router.post('/newOrder', rejectUnauthenticated, async (req, res) => {
-  // POST route code here
   try {
     const order = req.body;
     const orderArray = [
-      order.companyID, order.ingredientName, order.ingredientAmount, 
-      order.ingredientUnit, order.format, order.purity, order.dateManufactured, 
-      order.lotNumber, order.extractionMethod, order.city, order.state, order.country, 
-      order.harvestDate, order.cropStrain, order.sustainabilityInfo, order.shippedDate, 
-      order.carrierName, order.trackingNumber
+      order.companyID,
+      order.ingredientName,
+      order.ingredientAmount,
+      order.ingredientUnit,
+      order.format,
+      order.purity,
+      order.dateManufactured,
+      order.lotNumber,
+      order.extractionMethod,
+      order.city,
+      order.state,
+      order.country,
+      order.harvestDate,
+      order.cropStrain,
+      order.sustainabilityInfo,
+      order.shippedDate,
+      order.carrierName,
+      order.trackingNumber,
     ];
     const sqlText = `
     INSERT INTO "orders"
@@ -44,32 +56,35 @@ router.post('/newOrder', rejectUnauthenticated, async (req, res) => {
     `;
     await pool.query(sqlText, orderArray);
     res.sendStatus(200);
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err.message);
     res.sendStatus(500);
   }
 });
 
 router.post('/shipping', rejectUnauthenticated, async (req, res) => {
-  try{
+  try {
     const shipping = req.body;
     // console.log(shipping, "req.body")
-    
-    const shippingArray = [shipping.shippedDate, shipping.carrierName, shipping.trackingNumber];
-    console.log(shippingArray, "array")
-    const sqlText =  `
+
+    const shippingArray = [
+      shipping.shippedDate,
+      shipping.carrierName,
+      shipping.trackingNumber,
+    ];
+    console.log(shippingArray, 'array');
+    const sqlText = `
     INSERT INTO "orders"
       ("shippedDate", "carrierName", "trackingNumber")
     VALUES
       ($1, $2, $3);`;
-    
+
     await pool.query(sqlText, shippingArray);
     res.sendStatus(200);
-  }catch(err){
+  } catch (err) {
     console.error(err.message);
     res.sendStatus(500);
   }
-})
+});
 
 module.exports = router;
---
+++ src/components/CustomerDetail/CustomerDetail.jsx
@@ -0,0 +1,11 @@
+import { DialogContent, DialogContentText } from '@material-ui/core';
+
+function CustomerDetail(params) {
+  return (
+    <DialogContent>
+      <DialogContentText>This box</DialogContentText>
+    </DialogContent>
+  );
+}
+
+export default CustomerDetail;
