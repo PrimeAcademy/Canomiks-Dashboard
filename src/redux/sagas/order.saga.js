a3637e10db9e8e05df61fee4ffe5420126d9265c
George Papagapitos <george.g.papagapitos@gmail.com>
2021-04-15 13:36:00 -0500

    edit endpoint in order saga

--- src/redux/sagas/order.saga.js
+++ src/redux/sagas/order.saga.js
@@ -15,9 +15,9 @@ function* fetchCustomerOrders() {
 
 function* addOrder(action) {
   console.log("action payload,", action.payload)
-  try{
-    const response = yield axios.post('/api/orders/shipping', action.payload);
-  } catch(err) {
+  try {
+    const response = yield axios.post('/api/orders/newOrder', action.payload);
+  } catch (err) {
     console.error(err.message);
   }
 }
