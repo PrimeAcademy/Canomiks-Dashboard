675bc075e2c353bb797fb1623bc8eda1c7213e89
Vada Karlen <vada.karlen@gmail.com>
2021-04-18 14:15:16 -0500

    Reducer cleanup

--- src/redux/reducers/customer.reducer.js
+++ src/redux/reducers/customer.reducer.js
@@ -1,10 +1,10 @@
 const customerReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_CUSTOMER':
-        return action.payload;
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case 'SET_CUSTOMER':
+      return action.payload;
+    default:
+      return state;
   }
-  
-  export default customerReducer;
+};
+
+export default customerReducer;
--- src/redux/reducers/shipping.reducer.js
+++ src/redux/reducers/shipping.reducer.js
@@ -1,15 +1,12 @@
 const shippingReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'EDIT_SHIPPING':
-        return action.payload;
-      case 'UNSET_SHIPPING':
-        return [];
-      default:
-        return state;
-    }
-  };
-  
-  // user will be on the redux state at:
-  // state.user
-  export default shippingReducer;
-  
+  switch (action.type) {
+    case 'EDIT_SHIPPING':
+      return action.payload;
+    case 'CLEAR_SHIPPING':
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default shippingReducer;
