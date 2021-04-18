0ba5cc4fd9259899b91055e9a71a03415abef6b6
Vada Karlen <vada.karlen@gmail.com>
2021-04-18 11:46:43 -0500

    App cleaned up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,18 +6,20 @@ import {
   Switch,
 } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-// local components
+
+/* Local Components */
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+
 import AddSample from '../AddSample/AddSample';
 import Summary from '../Summary/Summary';
 import CustomerDashboard from '../CustomerDashboard/CustomerDashboard';
 import LabDashboard from '../LabDashboard/LabDashboard';
-import Manage from '../ManageCustomers/Manage';
+import ManageCustomers from '../ManageCustomers/Manage';
 import ShippingInfo from '../ShippingInfo/ShippingInfo';
 
 import './App.css';
@@ -44,6 +46,14 @@ function App() {
             <LandingPage />
           </ProtectedRoute>
 
+          <ProtectedRoute exact path="/login" authRedirect="/samples">
+            <LoginPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute exact path="/registration" authRedirect="/samples">
+            <RegisterPage />
+          </ProtectedRoute>
+
           <ProtectedRoute exact path="/samples">
             {user.authLevel === 'lab' || user.authLevel === 'admin' ? (
               <LabDashboard />
@@ -52,30 +62,23 @@ function App() {
             )}
           </ProtectedRoute>
 
-          <ProtectedRoute exact path="/login" authRedirect="/samples">
-            <LoginPage />
+          <ProtectedRoute path="/summary">
+            <Summary />
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/sample/add">
             <AddSample />
           </ProtectedRoute>
 
-          <ProtectedRoute exact path="/registration" authRedirect="/samples">
-            <RegisterPage />
-          </ProtectedRoute>
-
-          {user.authLevel === 'admin' && (
-            <ProtectedRoute exact path="/manage">
-              <Manage />
-            </ProtectedRoute>
-          )}
           <ProtectedRoute exact path="/sample/ship">
             <ShippingInfo />
           </ProtectedRoute>
 
-          <Route path="/summary">
-            <Summary />
-          </Route>
+          {user.authLevel === 'admin' && (
+            <ProtectedRoute exact path="/manage/customers">
+              <ManageCustomers />
+            </ProtectedRoute>
+          )}
         </Switch>
         <Footer />
       </Router>
