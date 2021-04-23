b2af3f9481642627d3d31b0da78646548d6c6911
George Papagapitos <george.g.papagapitos@gmail.com>
2021-04-23 11:41:25 -0500

    nav change

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -74,7 +74,7 @@ function Nav() {
             </NavLink>
           )}
 
-          {(user.authLevel !== 'admin' || user.authLevel !== 'lab') && (
+          {(user.authLevel !== 'admin' && user.authLevel !== 'lab') && (
             <>
               <a className="navLink" href="https://www.canomiks.com/contactus">
                 Help
