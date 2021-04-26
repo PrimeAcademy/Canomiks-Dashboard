82d8dd5a836789223538feb66f33bca03b03d5fb
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-04-26 08:26:48 -0500

    onchange whole companyfrom component is filled out

--- src/components/RegisterForm/CompanyForm.jsx
+++ src/components/RegisterForm/CompanyForm.jsx
@@ -18,6 +18,16 @@ function CompanyForm(props) {
   /* Store Imports */
   const errors = useSelector((store) => store.errors);
 
+  // function to make register faster for presentation
+  // one for each section/component
+  function presentationFormFill () {
+    props.setCompanyName('Golden Rays');
+    props.setCompanyAddress('567 Sunshine Drive');
+    props.setCompanyCity('Saint Louis');
+    props.setCompanyState('California');
+    props.setCompanyZip('33917');
+  }; // end presentationFormFill
+
   return (
     <form autoComplete="off" className={classes.root}>
       <Typography variant="h5" style={{ fontWeight: 600 }} gutterBottom>Company Information</Typography>
@@ -29,7 +39,8 @@ function CompanyForm(props) {
           variant="standard"
           size="small"
           value={props.companyName}
-          onChange={(event) => props.setCompanyName(event.target.value)}
+          onChange = {()=> presentationFormFill}
+          // onChange={(event) => props.setCompanyName(event.target.value)}
           required
         />
       </div>
@@ -40,7 +51,7 @@ function CompanyForm(props) {
           variant="standard"
           size="small"
           value={props.companyAddress}
-          onChange={(event) => props.setCompanyAddress(event.target.value)}
+          // onChange={(event) => props.setCompanyAddress(event.target.value)}
           required
         />
       </div>
@@ -51,7 +62,7 @@ function CompanyForm(props) {
           variant="standard"
           size="small"
           value={props.companyCity}
-          onChange={(event) => props.setCompanyCity(event.target.value)}
+          // onChange={(event) => props.setCompanyCity(event.target.value)}
           required
         />
       </div>
@@ -62,7 +73,7 @@ function CompanyForm(props) {
           variant="standard"
           size="small"
           value={props.companyState}
-          onChange={(event) => props.setCompanyState(event.target.value)}
+          // onChange={(event) => props.setCompanyState(event.target.value)}
           required
         />
       </div>
@@ -73,7 +84,7 @@ function CompanyForm(props) {
           variant="standard"
           size="small"
           value={props.companyZip}
-          onChange={(event) => props.setCompanyZip(event.target.value)}
+          // onChange={(event) => props.setCompanyZip(event.target.value)}
           required
         />
       </div>
